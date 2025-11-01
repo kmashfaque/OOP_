95d69bd9634e429b7dd7df9e578b9f88398866eb
ashfaque <rahamankmashfaque@gmail.com>
2025-11-01 21:06:58 +0600

    signup signin

--- oop.py
+++ oop.py
@@ -1,17 +1,18 @@
 class Employee:
 
-    def __init__(self, name, id, dept, salary, designation):
-        self.name = name 
-        self.id = id
-        self.dept = dept
-        self.salary = salary
-        self.designation = designation
+    def __init__(self):
+        self.name = "" 
+        self.id = ""
+        self.dept = ""
+        self.salary = ""
+        self.designation = ""
+        self.password = ""
         self.user()
 
     def user(self):
         user_input = input("""Welcome to chatbook. How would you like to proceed!!
                            1. Enter 1 to sign Up
-                           2. Enter 2 to sign In""")
+                           2. Enter 2 to sign In ==> """)
 
         if user_input == "1":
             self.signUp()
@@ -27,16 +28,15 @@ class Employee:
         print("Signup successfully")
 
     def signIn(self):
-        self.name = input("Enter you name: ")
-        self.password = input("Enter you password")
-        print("Sign in successfully.")
-         
-    
-    
-sam = Employee("Sam", 12, "IT", 35000, "Executive")
+        if not self.name and not self.password:
+            print("You need to signup first")
 
-# print(f"{sam.name} is our new employee. His id is {sam.id}. His department is {sam.dept} and salary is negotiated at {sam.salary}. His designation will be {sam.designation} ")
-# print(sam.id)
-# print(sam.dept)
-# print(sam.salary)
-# print(sam.designation)
+        else:
+            self.name = input("Enter you name: ")
+            self.password = input("Enter you password: ")
+            print("Sign in successfully.")
+            
+    
+    
+sam = Employee()
+
