19e923a29e3fa78ad4b6a1fe15f5a7e91a1a1894
ashfaque <rahamankmashfaque@gmail.com>
2025-11-04 01:13:09 +0600

    linkedlist insert at end

--- linkedlist.py
+++ linkedlist.py
@@ -24,12 +24,24 @@ class LinkedList:
             curr = curr.next
 
         print("None")
+    
+    def insertAtEnd(self, value):
+
+        node = Node(value)
+        curr = self.head
+        while curr.next is not None:
+            curr = curr.next
+        prev_last = curr.next
+        curr.next = node
+        node.next = prev_last
+
 
 ll = LinkedList()
 ll.insertAtBeginning(10)
 ll.insertAtBeginning(20)
 ll.insertAtBeginning(30)
-
+ll.insertAtEnd(40)
+ll.insertAtEnd(50)
 ll.print_linkedlist()
         
 
