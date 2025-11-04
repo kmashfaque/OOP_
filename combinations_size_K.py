85507dc003c0ae3ba7f850b7dec619b2f568d3b5
ashfaque <rahamankmashfaque@gmail.com>
2025-11-04 19:36:08 +0600

    combination size k

---
+++ combinations_size_K.py
@@ -0,0 +1,22 @@
+def combination_size(nums, k):
+    
+    result = []
+    new_arr = []
+    def combination(index):
+        
+        if len(new_arr) == k:
+            return result.append(new_arr.copy())
+
+        for i in range (index,len(nums)):
+
+            new_arr.append(nums[i])
+            combination(i+1)
+            new_arr.pop()
+
+    combination(0)
+    return result
+
+nums = [1,2,3,4]
+k = 2
+
+print(combination_size(nums, k))
