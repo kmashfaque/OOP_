d3e7d81268b7b855e191841a4c60e29977a9d64c
ashfaque <rahamankmashfaque@gmail.com>
2025-11-04 18:58:27 +0600

    permutations update

--- permutations.py
+++ permutations.py
@@ -1,19 +1,48 @@
-def permutation(arr):
-    result = []
+# def permutation(arr):
+#     result = []
     
 
-    def backtack(index):
-          if index == len(arr):
-               result.append(arr.copy())
-               return 
+#     def backtack(index):
+#           if index == len(arr):
+#                result.append(arr.copy())
+#                return 
           
-          for i in range(index, len(arr)):
-                arr[index], arr[i] = arr[i], arr[index]
-                backtack(index + 1)
-                arr[index], arr[i] = arr[i], arr[index]
+#           for i in range(index, len(arr)):
+#                 arr[index], arr[i] = arr[i], arr[index]
+#                 backtack(index + 1)
+#                 arr[index], arr[i] = arr[i], arr[index]
                 
-    backtack(0)
+#     backtack(0)
     
+#     return result
+
+# arr = [1,2,3] 
+# print(permutation(arr))
+
+
+def permutation(arr):
+    result = []
+
+    def backtrack(index):
+        print(f"ENTER index={index}, arr={arr}")
+
+        if index == len(arr):
+            print(">>> BASE CASE, append", arr)
+            result.append(arr.copy())
+            return
+
+        for i in range(index, len(arr)):
+            print(f"SWAP index={index} and i={i} -> {arr[index]} <-> {arr[i]}")
+            arr[index], arr[i] = arr[i], arr[index]
+
+            backtrack(index + 1)
+
+            print(f"UNDO SWAP index={index} and i={i} -> {arr[index]} <-> {arr[i]}")
+            arr[index], arr[i] = arr[i], arr[index]
+
+        print(f"EXIT index={index}, arr={arr}")
+
+    backtrack(0)
     return result
 
 arr = [1,2,3] 
